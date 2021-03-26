d68bba7759f5546020bd09f3e16956c334dc317e
Leia Quesada <leiaquesada@gmail.com>
2021-03-26 16:31:08 -0700

    Add search feature

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Contacts = () => {
   const [contacts, setContacts] = useState([]);
+  const [filterStr, setFilterStr] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:4000/")
@@ -11,19 +12,31 @@ const Contacts = () => {
 
   return (
     <>
+      <input
+        placeholder="search by last name"
+        value={filterStr}
+        type="text"
+        onChange={(e) => {
+          e.preventDefault();
+          let newStr = e.target.value;
+          setFilterStr(newStr);
+        }}
+      />
       <ul>
-        {contacts.map((contact) => {
-          return (
-            <ul key={contact.phone_number}>
-              <li>
-                {contact.first_name} {contact.last_name}
-              </li>
-              <li>Phone: {contact.phone_number}</li>
-              <li>Email: {contact.email}</li>
-              <hr />
-            </ul>
-          );
-        })}
+        {contacts
+          .filter((contact) => contact.last_name.includes(filterStr))
+          .map((contact) => {
+            return (
+              <ul key={contact.phone_number}>
+                <li>
+                  {contact.first_name} {contact.last_name}
+                </li>
+                <li>Phone: {contact.phone_number}</li>
+                <li>Email: {contact.email}</li>
+                <hr />
+              </ul>
+            );
+          })}
       </ul>
     </>
   );
