5912c86dbaae18729a96248d46a3e2dee8f944f4
Leia Quesada <leiaquesada@gmail.com>
2021-03-26 15:51:16 -0700

    AddContact component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Contacts from "./components/Contacts";
+import AddContact from "./components/AddContact";
 
 const App = () => {
   return (
     <>
       <h1>Address Book</h1>
       <Contacts />
+      <AddContact />
     </>
   );
 };
--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
-import favicon from "./favicon.png";
+import favicon from "../favicon.png";
 
 const AddContact = () => {
   return (
